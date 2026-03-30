6c9988a66dbd5b6be77648aaf5c50f02d9b5ea80
vasia2808 <vasia2808@gmail.com>
2026-03-30 20:01:15 +0300

    Added GTK theme and icons

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ RICE_CONFIG_DIR=$RICE_DIR/dotfiles/.config
 
 [ -d "$XDG_CONFIG_HOME" ] && CONFIG_DIR=$XDG_CONFIG_HOME || CONFIG_DIR=$HOME/.config
 
+THEMES_DIR=$HOME/.local/share/themes
+ICONS_DIR=$HOME/.local/share/icons
+
 sudo pacman -Syu --needed --noconfirm alacritty engrampa fastfetch firefox ffmpegthumbnailer gvfs-mtp grim hypridle hyprland hyprlock hyprpaper mako neovim noto-fonts-cjk otf-font-awesome pavucontrol paru pipewire-jack pipewire-pulse playerctl polkit-gnome qimgv ranger rofi-wayland thunar thunar-archive-plugin tumbler ttf-jetbrains-mono-nerd waybar wl-clipboard wl-clip-persist xdg-desktop-portal-hyprland zsh
 
 if [ ! -d "$CONFIG_DIR" ]; then
@@ -36,6 +39,22 @@ ln -s "$RICE_DIR/dotfiles/.zshenv" "$HOME/.zshenv"
 ln -s "$RICE_DIR/dotfiles/.zshrc" "$HOME/.zshrc"
 
 chsh -s /usr/bin/zsh
+
+if [ ! -d "$THEMES_DIR" ]; then
+    echo Creating themes directory
+    mkdir -p "$THEMES_DIR"
+fi
+
+if [ ! -d "$ICONS_DIR" ]; then
+    echo Creating icons directory
+    mkdir -p "$ICONS_DIR"
+fi
+
+ln -s "$RICE_DIR/GTK/Material-Black-Cherry-BE-Custom" "$HOME/.local/share/themes/Material-Black-Cherry-BE-Custom"
+ln -s "$RICE_DIR/GTK/Material-Black-Cherry-Numix-Custom" "$HOME/.local/share/icons/Material-Black-Cherry-Numix-Custom"
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Material-Black-Cherry-BE-Custom'
+gsettings set org.gnome.desktop.interface icon-theme 'Material-Black-Cherry-Numix-Custom'
 
 sudo ln -s /usr/bin/alacritty /usr/bin/konsole
 
