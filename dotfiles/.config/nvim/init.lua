fe6f73a99887bb9fcdd3a2ee5aaaf02684b97839
vasia2808 <vasia2808@gmail.com>
2026-04-09 11:18:50 +0300

    Moved neovim settings from init.vim to init.lua file

---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.termguicolors = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.guicursor = 'n-v-o-r-cr:block-blinkoff600-blinkon600,i-c-ci-ve:ver20-blinkoff600-blinkon600'
+
